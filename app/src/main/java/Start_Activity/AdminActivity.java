b50cd428e2bd69967998d413000db3521be89511
Sruf-y <andreibotofan@gmail.com>
2025-03-24 19:18:06 +0200

    Cautare finished

--- app/src/main/java/Start_Activity/AdminActivity.java
+++ app/src/main/java/Start_Activity/AdminActivity.java
@@ -2,12 +2,17 @@ package Start_Activity;
 
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.ProiectSI.R;
+
+import Setari_Admin_Angajat.Creare_Cont_Angajat;
 
 
 public class AdminActivity extends AppCompatActivity {
@@ -15,14 +20,10 @@ public class AdminActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // enableEdgeToEdge();
+
         setContentView(R.layout.activity_admin);
 
-        // ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-        //     val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-        //     v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-        //     return insets;
-        // });
+
 
         String angajatName = getIntent().getStringExtra("arg1");
         String angajatPassword = getIntent().getStringExtra("arg2");
@@ -31,7 +32,41 @@ public class AdminActivity extends AppCompatActivity {
 
         // AICI SCRII CODUL
 
+        Button buton_creare_angajat = findViewById(R.id.admin_buton_adaugare_angajat);
+
+        if(isAdmin) {
+
+            buton_creare_angajat.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent_nou = new Intent(AdminActivity.this, Creare_Cont_Angajat.class);
+                    startActivity(intent_nou);
+                }
+            });
+
+
+        }
+        else{
+            buton_creare_angajat.setVisibility(View.GONE);
+        }
+
+
+
+
+
 
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
