e4d5cc71b6277e7493c9144a8e003265b9dddc99
Sruf-y <andreibotofan@gmail.com>
2025-03-25 17:04:59 +0200

    Botofan: finisaje

---
+++ app/src/main/java/Functii_Utils/Functii.kt
@@ -0,0 +1,33 @@
+package Functii_Utils
+
+import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.File
+
+class Functii {
+
+    companion object {
+        fun <T> SaveAsJson(context: Context, filename: String, data: T) {
+            val json = Gson().toJson(data);
+            val filepath = context.filesDir.toString() + "/" + filename + ".json"
+
+            File(filepath).writeText(json)
+        }
+
+        fun <T> LoadFromJson(context: Context, filename: String, data: T): T {
+
+            val filepath = context.filesDir.toString() + "/" + filename + ".json"
+            val file = File(filepath)
+
+            if (file.exists()) {
+                val loadedFile = file.readText()
+                val type = object : TypeToken<T>() {}.type
+                val readData: T = Gson().fromJson(loadedFile, type)
+
+                return readData
+            }
+            return data
+        }
+    }
+}
