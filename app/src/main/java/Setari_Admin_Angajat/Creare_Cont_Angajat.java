18a2c9b3e17a2e9cb380c43da3ce8043e02c270b
Darius2602 <darius.bologa@student.upt.ro>
2025-04-01 15:56:45 +0300

    Am creat pagina pentru adaugare cont angajat cu UI si cod

--- app/src/main/java/Setari_Admin_Angajat/Creare_Cont_Angajat.java
+++ app/src/main/java/Setari_Admin_Angajat/Creare_Cont_Angajat.java
@@ -1,6 +1,9 @@
 package Setari_Admin_Angajat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,16 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.ProiectSI.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import DataClasses.Angajat;
+import DataClasses.GlobalVars;
+import Functii_Utils.Functii;
+
+
+
+
 public class Creare_Cont_Angajat extends AppCompatActivity {
 
     @Override
@@ -18,9 +31,40 @@ public class Creare_Cont_Angajat extends AppCompatActivity {
         setContentView(R.layout.activity_creare_cont_angajat);
         //aici cod
 
+        EditText etUsername = findViewById(R.id.et_username);
+        EditText etPassword = findViewById(R.id.et_password);
+        Button btnAdauga = findViewById(R.id.btn_adauga);
 
+        btnAdauga.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
 
+                Angajat nouAngajat = new Angajat(username, password);
+                adaugaAngajat(nouAngajat);
+            }
+        });
+    }
+    private void adaugaAngajat(Angajat angajat) {
+        List<Angajat> listaAngajati = GlobalVars.INSTANCE.getLista_Angajati();
 
+        if (listaAngajati == null) {
+            listaAngajati = new ArrayList<>();
+        }
+
+        listaAngajati.add(angajat);
+
+        GlobalVars.INSTANCE.setLista_Angajati((ArrayList<Angajat>) listaAngajati);
+
+        Functii.Companion.SaveAsJson(this, "Lista_Angajati", listaAngajati);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        List<Angajat> listaAngajati = Functii.Companion.LoadFromJson(this, "Lista_Angajati", new ArrayList<>(), ArrayList.class);
+        GlobalVars.INSTANCE.setLista_Angajati((ArrayList<Angajat>) listaAngajati);
 
     }
 }
