0bf476e6c5e5e0900f9ca4dd96d13ca39dcec762
Sruf-y <andreibotofan@gmail.com>
2025-05-14 23:27:58 +0300

    Comit

--- app/src/main/java/DataClasses/Comanda.kt
+++ app/src/main/java/DataClasses/Comanda.kt
@@ -3,9 +3,11 @@ package DataClasses
 import android.os.Parcelable
 import com.ProiectSI.KotlinUtils.Companion.toBoolean
 import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Serializable
 import kotlin.random.Random
 
 @Parcelize
+@Serializable
 class Comanda(var id:String =randomizeID(),
               var list:ArrayList<Meniu_Item> = ArrayList<Meniu_Item>(),
               var listNumberOfs:ArrayList<Int> = ArrayList<Int>(),
@@ -51,18 +53,19 @@ class Comanda(var id:String =randomizeID(),
         fun randomizeID(): String {
             val randomizer = Random
 
-            val alphabet = listOf("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+            val alphabetString = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-            var string =""
+            var string = ""
 
-            do{
-                string =""
+            do {
+                string = ""
 
-                for(i in 0..5){
-                    string+=alphabet[randomizer.nextInt(0,alphabet.size-1)]
+                for (i in 0..5) {
+                    // Generate a random index within the bounds of the alphabetString
+                    val randomIndex = randomizer.nextInt(0, alphabetString.length)
+                    string += alphabetString[randomIndex]
                 }
-            }while (GlobalVars.lista_Comenzi.map { p->p.id }.contains(string))
-
+            } while (GlobalVars.lista_Comenzi.map { p -> p.id }.contains(string))
 
             return string
         }
